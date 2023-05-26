75949717b99e9c370bd888da6e5df035827ae260
lixiaowei11111 <lixiaowei11111@users.noreply.github.com>
2023-05-26 15:33:31 +0000

    deploy: 44f354607d89ba4775e62162bb6d3ad5e306c886

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./App.less";
-
-function App() {
-	return <>App </>;
-}
-
-export default App;
--- src/typings/file.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.less";
-declare module "*.css";
-declare module "*.svg";
-declare module "*.png";
-declare module "*.json";
