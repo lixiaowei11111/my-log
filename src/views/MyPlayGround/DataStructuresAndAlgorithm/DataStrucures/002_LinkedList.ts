5213ce9f0d8e8b1ce6bb08266772c76fc2f9bebf
Matryoshka <17673942507@163.com>
2023-09-12 19:01:39 +0800

    feat: linked list LRUCache completed

--- src/views/MyPlayGround/DataStructuresAndAlgorithm/DataStrucures/002_LinkedList.ts
+++ src/views/MyPlayGround/DataStructuresAndAlgorithm/DataStrucures/002_LinkedList.ts
@@ -1,29 +1,92 @@
 // 1. 链表的种类有 单向链表， 循环链表， 双向链表，以及双向循环链表
+
+import { ValueOptions } from "postcss/lib/container";
+
 // 使用双向循环链表来实现一个LRU缓存算法
-class ListNode<T> {
-	key: number;
-	value: T;
-	next: ListNode<T> | null;
-	constructor(key: number, value: T) {
+class Node<K, V> {
+	key: K;
+	value: V;
+	prev: Node<K, V> | null;
+	next: Node<K, V> | null;
+	constructor(key: K, value: V) {
 		this.key = key;
 		this.value = value;
+		this.prev = null;
 		this.next = null;
 	}
 }
 
-class LRUCache<T> {
-	private capacity: number; // LRUCache容量
-	private cache: Map<number, ListNode<T>>;
-	private head: ListNode<T>;
-	private tail: ListNode<T>;
+class LRUCache<K, V> {
+	capacity: number;
+	cache: Map<K, Node<K, V>>;
+	head: Node<K, V> | null;
+	tail: Node<K, V> | null;
+
 	constructor(capacity: number) {
 		this.capacity = capacity;
-		this.cache = new Map();
-		this.head = new ListNode(0, null as unknown as T);
-		this.tail = new ListNode(0, null as unknown as T);
-		this.head.next = this.tail;
-		this.tail.next = null;
+		this.cache = new Map<K, Node<K, V>>();
+		this.head = null;
+		this.tail = null;
 	}
 
-	// private removeNode
+	private addToHead(node: Node<K, V>) {
+		node.prev = null;
+		node.next = this.head;
+
+		if (this.head) {
+			this.head.prev = node;
+		} else {
+			this.tail = node;
+		}
+
+		this.head = node;
+	}
+
+	private removeNode(node: Node<K, V>) {
+		if (node.prev) {
+			node.prev.next = node.next;
+		} else {
+			this.head = node.next;
+		}
+
+		if (node.next) {
+			node.next.prev = node.prev;
+		} else {
+			this.tail = node.prev;
+		}
+	}
+
+	private moveToHead(node: Node<K, V>) {
+		this.removeNode(node);
+		this.addToHead(node);
+	}
+
+	get(key: K): V | undefined {
+		const node = this.cache.get(key);
+
+		if (node) {
+			this.moveToHead(node);
+			return node.value;
+		}
+
+		return undefined;
+	}
+
+	put(key: K, value: V) {
+		const node = this.cache.get(key);
+
+		if (node) {
+			node.value = value;
+			this.moveToHead(node);
+		} else {
+			const newNode = new Node(key, value);
+			this.cache.set(key, newNode);
+			this.addToHead(newNode);
+
+			if (this.cache.size > this.capacity) {
+				this.cache.delete(this.tail!.key);
+				this.removeNode(this.tail!);
+			}
+		}
+	}
 }
